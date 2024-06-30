12d6cd5dada987c7468b7a9a00b634712066cdf6
hernan-vela <hernan.velamar@gmail.com>
2024-06-30 19:44:13 +1000

    Debugging of CSV and JSON files

--- test_project/src/csv_file.py
+++ test_project/src/csv_file.py
@@ -12,7 +12,7 @@ with open('euro_cup_matches.csv', 'r') as file:
     csv_reader = csv.DictReader(file)
     matches_above_3_goals = [row for row in csv_reader if int(row['Score A']) + int(row['Score B']) > 3]
 
-fieldnames = ['Team A', 'Tema B', 'Score A', 'Score B', 'Date']
+fieldnames = ['Team A', 'Team B', 'Score A', 'Score B', 'Date']
 with open('high_scoring_matches.csv', 'w', newline='') as file:
     csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
     csv_writer.writeheader()
